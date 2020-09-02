c1b1c380188fca3cec8b5b39088f3e721bdfddc8
Arnav Sirigere <arnavsirigere@gmail.com>
2020-09-02 15:51:13 +0530

    🎇 Initial Commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => console.log('Discord Bot is starting!'));
+
+client.on('message', async (message) => {
+  if (message.channel.id == process.env.PUBLIC_COC_CHANNEL_ID) {
+    const { content, author } = message;
+    const { username } = author;
+    const avatarUrl = author.avatarURL();
+    const cocLogsChannel = message.guild.channels.cache.find((ch) => ch.id == process.env.MOD_COC_LOGS);
+    const embed = new MessageEmbed().setColor('#FF6663').setAuthor(username, avatarUrl).setDescription(content);
+    await cocLogsChannel.send(embed);
+    await message.delete();
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
